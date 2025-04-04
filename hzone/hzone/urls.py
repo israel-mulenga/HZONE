3a999459a26b18b09034808f99030884864bc249
israel <mulengaisrael5@gmail.com>
2025-04-04 08:51:10 +0200

    landing_page_init

--- hzone/hzone/urls.py
+++ hzone/hzone/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hzone_app import views
 
 urlpatterns = [
+    path("", views.landing, name="landing"),
     path('admin/', admin.site.urls),
 ]
